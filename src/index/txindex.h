ebf98f5930dafd99d9ad5164bce272266b0e6d0f
Mach Deng Mach <freedom.hotel09@gmail.com>
2025-03-04 10:56:25 +0200

    Update txindex.h

--- src/index/txindex.h
+++ src/index/txindex.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_INDEX_TXINDEX_H
-#define BITCOIN_INDEX_TXINDEX_H
+#ifndef JUCOIN_INDEX_TXINDEX_H
+#define JUCOIN_INDEX_TXINDEX_H
 
 #include <index/base.h>
 
